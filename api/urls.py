3d56e9f86b9f9266e7426df9a34a4b3473f8e204
Akram Mukasa <mukasaakram55@gmail.com>
2018-08-15 18:30:21 +0300

    get one question urls added.[#159759259]

--- api/urls.py
+++ api/urls.py
@@ -14,3 +14,6 @@ class GetUrls:
         """
         questions_view = GetAllquestion.as_view('questions')
         question.add_url_rule('/ui/api/questions/', view_func=questions_view, defaults={'question_id': None}, methods=['GET',])
+        question.add_url_rule('/ui/api/questions/<int:question_id>', view_func=questions_view, methods=['GET',])
+        question_view_update = (GetAllquestion.as_view('rides_update')
+        question.add_url_rule('/ui/api/questions/', defaults={'question_id': None}, view_func=questions_view_update, methods=['POST',])
