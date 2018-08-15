4514a56f08b0680102b41ee85a2d963f624af73e
Akram Mukasa <mukasaakram55@gmail.com>
2018-08-15 16:52:42 +0300

    included

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+"""
+Module decorates views to urls
+"""
+from views import GetAllquestion
+
+class GetUrls:
+    """
+    Method that views with urls
+    """
+    @staticmethod
+    def fetch_urls(question):
+        """
+        Method that views with urls
+        """
+        questions_view = GetAllquestion.as_view('questions')
+        question.add_url_rule('/ui/api/questions/', view_func=questions_view, defaults={'question_id': None}, methods=['GET',])
+        question.add_url_rule('/ui/api/questions/<int:question_id>', view_func=questions_view, methods=['GET',])
