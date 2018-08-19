9ad37be731cec3cb398534b7f478e8aa9bf5c4e7
Akram Mukasa <mukasaakram55@gmail.com>
2018-08-19 16:04:51 +0300

    edit files

--- api/app.py
+++ api/app.py
@@ -2,11 +2,11 @@
 This is the main module
 """
 from flask import Flask
-from urls import GetUrls
+from url import GetUrls
 APP = Flask(__name__)
 APP.env = 'development'
 APP.testing = True
 GetUrls.fetch_urls(APP)
 
 if __name__ == '__main__':
-    APP.run(debug= True)
+    APP.run(debug=True)
--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,13 @@ class GetUrls:
         Method that views with urls
         """
         questions_view = GetAllquestion.as_view('questions')
-        question.add_url_rule('/ui/api/questions/', view_func=questions_view, defaults={'question_id': None}, methods=['GET',])
-        question.add_url_rule('/ui/api/questions/<int:question_id>', view_func=questions_view, methods=['GET',])
-        question.add_url_rule('/ui/api/questions/', view_func=questions_view, methods=['POST',])
-        question.add_url_rule('/ui/api/questions/<int:question_id>/answers/', view_func=GetAllanswers.as_view('answer'),defaults={'question_id': None},  methods=['POST',])
+        question.add_url_rule('/ui/api/questions/',
+                              view_func=questions_view, defaults={'question_id': None},
+                              methods=['GET',])
+        question.add_url_rule('/ui/api/questions/<int:question_id>',
+                              view_func=questions_view, methods=['GET',])
+        question.add_url_rule('/ui/api/questions/',
+                              view_func=questions_view, methods=['POST',])
+        question.add_url_rule('/ui/api/questions/<int:question_id>/answers/',
+                              view_func=GetAllanswers.as_view('answer'),
+                              defaults={'question_id': None}, methods=['POST',])
